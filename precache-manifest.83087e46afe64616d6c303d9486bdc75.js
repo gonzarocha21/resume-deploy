dbf1e83749f7983b554cc30fb36abbfee1c07659
gonzarocha21 <gonzarocha21@gmail.com>
2020-10-20 22:00:15 -0300

    Updates

---
+++ precache-manifest.83087e46afe64616d6c303d9486bdc75.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc6d97867fdf21feecb5b613f091e032",
+    "url": "/github.io/cv-deply/index.html"
+  },
+  {
+    "revision": "07fe0d1a5daa984a9d9d",
+    "url": "/github.io/cv-deply/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "221c244e662c164cb87e",
+    "url": "/github.io/cv-deply/static/css/main.cd95e21b.chunk.css"
+  },
+  {
+    "revision": "07fe0d1a5daa984a9d9d",
+    "url": "/github.io/cv-deply/static/js/2.70929763.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/github.io/cv-deply/static/js/2.70929763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "221c244e662c164cb87e",
+    "url": "/github.io/cv-deply/static/js/main.9f32c46c.chunk.js"
+  },
+  {
+    "revision": "e77bdace9a86ccda0d66",
+    "url": "/github.io/cv-deply/static/js/runtime-main.689535da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/cv-deply/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "50c8eabfe50759295d62e6fd4766c277",
+    "url": "/github.io/cv-deply/static/media/mi-foto.50c8eabf.png"
+  }
+]);
